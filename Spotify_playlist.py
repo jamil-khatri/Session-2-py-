61f79a737aecbc9fb6ccd9a0f1dd252aec287639
jamil-khatri <jkhatri37capp@gmail.com>
2026-09-17 14:54:40 +0530

    Create Spotify_playlist.py

---
+++ Spotify_playlist.py
@@ -0,0 +1,12 @@
+playlist_title = "Driving_playlist"
+number_of_songs = 137
+is_public = True
+
+print("Playlist Title:", playlist_title)
+print("Memory address:", id(playlist_title))
+
+print("Number of Songs:", number_of_songs)
+print("Memory address:", id(number_of_songs))
+
+print("Is Public:", is_public)
+print("Memory address:", id(is_public))
