0df3eaa17bda823e8101583bc8ef0396fd65d3bb
jamil-khatri <jkhatri37capp@gmail.com>
2026-09-17 14:50:17 +0530

    Create Flipcart_product.py

---
+++ Flipcart_product.py
@@ -0,0 +1,9 @@
+product_name = "nike shoes"
+price = 2399
+is_available = True
+rating = 4.5
+
+print(product_name, type(product_name))
+print(price, type(price))
+print(is_available, type(is_available))
+print(rating, type(rating))
