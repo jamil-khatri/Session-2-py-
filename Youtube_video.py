c8b267dd5fc3e81d28bf3def93b3bd37eef048ea
jamil-khatri <jkhatri37capp@gmail.com>
2026-09-17 15:01:47 +0530

    Create Youtube_video.py

---
+++ Youtube_video.py
@@ -0,0 +1,20 @@
+title = "Python Tutorial"
+views = 118635
+likes = 3453
+is_premium = False
+comments = ["Very helpful!", "Great tutorial!", "Thanks!"]
+
+# title is a primitive data type because it stores a single text value.
+# Its data type is str.
+
+# views is a primitive data type because it stores a single number.
+# Its data type is int.
+
+# likes is a primitive data type because it stores a single number.
+# Its data type is int.
+
+# is_premium is a primitive data type because it stores a True or False value.
+# Its data type is bool.
+
+# comments is a non-primitive data type because it is a list
+# that can store multiple values.
